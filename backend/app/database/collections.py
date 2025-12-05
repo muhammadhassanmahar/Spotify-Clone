4c0902d41b4f1f47ab5df84f47f61af58a889dbc
Muhammad Hassan <immaharhasaan@gmail.com>
2025-12-05 10:17:29 +0500

    Update

---
+++ backend/app/database/collections.py
@@ -0,0 +1,13 @@
+# --------------------------------------------------------
+# All MongoDB collection names used in the application
+# --------------------------------------------------------
+
+USERS_COLLECTION = "users"
+ARTISTS_COLLECTION = "artists"
+ALBUMS_COLLECTION = "albums"
+SONGS_COLLECTION = "songs"
+PLAYLISTS_COLLECTION = "playlists"
+GENRES_COLLECTION = "genres"
+HISTORY_COLLECTION = "history"
+LIKED_SONGS_COLLECTION = "liked_songs"
+RECENT_COLLECTION = "recent"
