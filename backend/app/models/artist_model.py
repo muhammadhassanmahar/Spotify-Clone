1d3f7359651b658079975644429f1c70bc0cb7b6
Muhammad Hassan <immaharhasaan@gmail.com>
2025-12-05 10:21:01 +0500

    Update

---
+++ backend/app/models/artist_model.py
@@ -0,0 +1,44 @@
+from datetime import datetime
+from bson import ObjectId
+from app.database.connection import db
+from app.database.collections import ARTISTS_COLLECTION
+
+
+class ArtistModel:
+    """
+    Artist model for storing artist information:
+    - name
+    - bio
+    - image
+    - social links
+    """
+
+    @staticmethod
+    async def create_artist(data: dict):
+        data["created_at"] = datetime.utcnow()
+        data["updated_at"] = datetime.utcnow()
+        result = await db[ARTISTS_COLLECTION].insert_one(data)
+        return str(result.inserted_id)
+
+    @staticmethod
+    async def get_all_artists():
+        cursor = db[ARTISTS_COLLECTION].find({})
+        return await cursor.to_list(length=1000)
+
+    @staticmethod
+    async def find_by_id(artist_id: str):
+        return await db[ARTISTS_COLLECTION].find_one({"_id": ObjectId(artist_id)})
+
+    @staticmethod
+    async def update_artist(artist_id: str, update_data: dict):
+        update_data["updated_at"] = datetime.utcnow()
+        result = await db[ARTISTS_COLLECTION].update_one(
+            {"_id": ObjectId(artist_id)},
+            {"$set": update_data}
+        )
+        return result.modified_count > 0
+
+    @staticmethod
+    async def delete_artist(artist_id: str):
+        result = await db[ARTISTS_COLLECTION].delete_one({"_id": ObjectId(artist_id)})
+        return result.deleted_count > 0
