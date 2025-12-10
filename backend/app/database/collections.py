5baadf2d3f1ff539a417974150c14bff169b2c7d
Muhammad Hassan <immaharhasaan@gmail.com>
2025-12-10 15:34:34 +0500

    Update

--- backend/app/database/collections.py
+++ backend/app/database/collections.py
@@ -1,6 +1,4 @@
-# --------------------------------------------------------
-# All MongoDB collection names used in the application
-# --------------------------------------------------------
+from .connection import db
 
 USERS_COLLECTION = "users"
 ARTISTS_COLLECTION = "artists"
@@ -11,3 +9,14 @@ GENRES_COLLECTION = "genres"
 HISTORY_COLLECTION = "history"
 LIKED_SONGS_COLLECTION = "liked_songs"
 RECENT_COLLECTION = "recent"
+
+# MongoDB collection objects
+users_collection = db[USERS_COLLECTION]
+artists_collection = db[ARTISTS_COLLECTION]
+albums_collection = db[ALBUMS_COLLECTION]
+songs_collection = db[SONGS_COLLECTION]
+playlists_collection = db[PLAYLISTS_COLLECTION]
+genres_collection = db[GENRES_COLLECTION]
+history_collection = db[HISTORY_COLLECTION]
+liked_songs_collection = db[LIKED_SONGS_COLLECTION]
+recent_collection = db[RECENT_COLLECTION]
