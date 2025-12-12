0cc337be32ab082492ef2dc14e625711f56dd250
Muhammad Hassan <immaharhasaan@gmail.com>
2025-12-12 15:24:06 +0500

    Update

--- backend/app/models/recent_model.py
+++ backend/app/models/recent_model.py
@@ -1,12 +1,9 @@
-from sqlalchemy import Column, Integer, DateTime
-from sqlalchemy.sql import func
-from app.database import Base
+from pydantic import BaseModel
+from datetime import datetime
 
 
-class RecentSong(Base):
-    __tablename__ = "recent_songs"
-
-    id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, index=True, nullable=False)
-    song_id = Column(Integer, index=True, nullable=False)
-    played_at = Column(DateTime(timezone=True), server_default=func.now())
+class RecentSong(BaseModel):
+    id: str | None = None
+    user_id: int
+    song_id: int
+    played_at: datetime | None = None
